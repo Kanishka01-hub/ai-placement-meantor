84822c7c282532e2b6e465d31a19a86ee152d532
Kanishka <kanishkasinha0103@gmail.com>
2026-05-29 19:25:28 +0530

    Prepared project for deployment

--- backend/server.js
+++ backend/server.js
@@ -7,34 +7,24 @@ const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use("/api/auth", authRoutes);
 
-// Test Route
-app.post("/test", (req, res) => {
-  res.json({ message: "POST working" });
-});
-
-// Home Route
 app.get("/", (req, res) => {
   res.send("Server is running successfully");
 });
 
-// MongoDB Connection
 mongoose
-  .connect(process.env.MONGO_URI, {
-    serverSelectionTimeoutMS: 5000,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected");
 
-    // Start Server
-    app.listen(5000, () => {
-      console.log("Server started on port 5000");
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, () => {
+      console.log(`Server started on port ${PORT}`);
     });
   })
   .catch((err) => {
